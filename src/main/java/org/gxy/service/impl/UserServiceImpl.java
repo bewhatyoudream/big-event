6ef78e14fc9f7289d934b487317739bea6fe455c
sunshuwei <371360082@qq.com>
2024-05-23 16:33:42 +0800

    更新用户头像

--- src/main/java/org/gxy/service/impl/UserServiceImpl.java
+++ src/main/java/org/gxy/service/impl/UserServiceImpl.java
@@ -4,10 +4,13 @@ import org.gxy.mapper.UserMapper;
 import org.gxy.pojo.User;
 import org.gxy.service.UserService;
 import org.gxy.utils.Md5Util;
+import org.gxy.utils.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -32,5 +35,12 @@ public class UserServiceImpl implements UserService {
     public void update(User user) {
         user.setUpdateTime(LocalDateTime.now());
         userMapper.update(user);
+    }
+
+    @Override
+    public void updateAvatar(String avatarUrl) {
+        Map<String, Object> map = ThreadLocalUtil.get();
+        Integer id = (Integer) map.get("id");
+        userMapper.updateAvatar(avatarUrl, id);
     }
 }
